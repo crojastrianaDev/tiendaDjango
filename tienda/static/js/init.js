95afaab3ff306ca8cd23c1ed0c4ad4a6dba04be5
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-07-29 15:03:19 -0500

    Agregando ststics

---
+++ tienda/static/js/init.js
@@ -0,0 +1,16 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true
+    });
+
+    $('.slider').slider();
+    $('.tabs').tabs();
+
+  })(jQuery); // end of jQuery name space
