e62b9fd7eec008fe736268ddefd08105cf48fea4
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-07-29 15:32:57 -0500

    Modificación archivos

--- tienda/users/apps.py
+++ tienda/users/apps.py
@@ -6,8 +6,4 @@ class UsersConfig(AppConfig):
     name = "tienda.users"
     verbose_name = _("Users")
 
-    def ready(self):
-        try:
-            import tienda.users.signals  # noqa F401
-        except ImportError:
-            pass
+   
