c3c4313c6d38b1f1a3a21ee044f32f3d8d5f376c
crojastriana3 <cristianrojas@unimayor.edu.co>
2020-07-29 17:20:57 -0500

    Error de views solucionado

--- tienda/users/views.py
+++ tienda/users/views.py
@@ -1,191 +1,152 @@
-from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView, RedirectView, UpdateView, TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView, TemplateView, CreateView, DeleteView
 from django.contrib.auth.views import LoginView, LogoutView
-from tienda.users.forms import FormularioRegistro 
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
-# se importan los modulos de consulta django
+
+
 from django.db.models import Q, Max, Min
-from tienda.productos.models import Producto, Comentario, Carrito
+
+
+
+from tienda.productos.models import Producto, Comentario, CarritoCompras
 
 User = get_user_model()
 
 
-#creamos el view PRINCIPAL
-
 class Indice(TemplateView):
     template_name = 'index.html'
 
-#CLASE PARA LISTAR LOS PRODUCTOS
-class ListadoProductos(ListView):
-    #template render
-    print("Pase a listado productos")
+class ListadoProducto(ListView):
     template_name = 'listado_productos.html'
-    #el modelo
     model = Producto
-    #cada cuanto se pagina los productos
-    paginate_by = 4
-    #obtiene todos los objetos en esa base de datos que pertenezcan a ese modelo.
+    paginate_by = 10
+
     def get_queryset(self):
-        query = None #si viene algun nombre filtrando con el nombre
-        #atravez del reques traemos cada dato con get
-        if('nombre' in self.request.GET) and self.request.GET['nombre'] != "":
-            #query ahora es igual a q. y respondemos el objeto de ese nombre
+        query = None
+
+        if ('nombre' in self.request.GET) and self.request.GET['nombre'] != "":
             query = Q(nombre=self.request.GET["nombre"])
-        #ahora consultamos con los precios
-        if ('maximo' in self.request.GET) and self.request.GET['maximo'] !="":
+
+        if ('maximo' in self.request.GET) and self.request.GET['maximo'] != "":
             try:
-                if query == None: #si viene sin nombre lo hacemos con el maximo
-                    #lo que hacemos es sacar el número y buscar lo que sea menor o igual a ese número
+                if query == None:
                     query = Q(precio__lte=int(float(self.request.GET['maximo'])))
                 else:
                     query = query & Q(precio__lte=int(float(self.request.GET['maximo'])))
-            except:  #si insertan letras no ocurre nada  
-              pass
-        if ('minimo' in self.request.GET) and self.request.GET['minimo'] !="":
+            except:
+                pass
+
+
+        if ('minimo' in self.request.GET) and self.request.GET['minimo'] != "":
             try:
-                if query == None: #si viene sin nombre lo hacemos con el maximo
-                    #lo que hacemos es sacar el número y buscar lo que sea mayor o igual a ese número
+                if query == None:
                     query = Q(precio__gte=int(float(self.request.GET['minimo'])))
                 else:
                     query = query & Q(precio__gte=int(float(self.request.GET['minimo'])))
-            except:  #si insertan letras no ocurre nada  
-              pass
-        # finalmente preguntamos que si query es no none  -- traemos los productos
+            except:
+                pass
+
+
         if query is not None:
             productos = Producto.objects.filter(query)
-        else: #agarra todos los productos si no los filtra los entrega todos
+        else:
             productos = Producto.objects.all()
         return productos
-
-    #con este enviamos info que se requiera.
+    
     def get_context_data(self, **kwargs):
-        #enviamos a listado producto traeme lo que tiene listview
-        context = super(ListadoProductos, self).get_context_data(**kwargs)
-        #agreamos los campos de minimo y maximo y entregara esto el producto con menor o maximo
+        context = super(ListadoProducto, self).get_context_data(**kwargs)
         context['maximo'] = Producto.objects.all().aggregate(Max('precio'))['precio__max']
         context['minimo'] = Producto.objects.all().aggregate(Min('precio'))['precio__min']
-        print(context)
         return context
+
 
 class DetalleProducto(DetailView):
     template_name = 'detalle.html'
-    model = Producto    
+    model = Producto
 
 class ComentarioProducto(CreateView):
     template_name = 'detalle.html'
     model = Comentario
-    #los datos del comentario sin el fecha que es automatica
-    fields = ('comentario','usuario','productofk',)
-# nos redirecciona el mensaje a el producto con su llave 
-    def get_success_url(self):  
-        #devolvemos a detalle producto con la pk del producto para recargar con mensajes
-        return "/detalleProducto/{}/".format(self.object.productofk.pk)
-
-class Salir(LogoutView):
-    #me envia redireccionado a la inicial
-   
-    next_page = reverse_lazy('indice')
-
-class Ingresar(LoginView):
-    #llamaos login
-    template_name = 'login.html'
-
-    def get(self, reques, *args, **kwargs):
-        #este metodo si ya nos autenticamos y quiero entrar de nuevo me manda a indice
-        #si no, me loguea
-        if reques.user.is_authenticated:
-            return HttpResponseRedirect(reverse('indice'))
-        else:
-            context = self.get_context_data(**kwargs)
-            return self.render_to_response(context)
+    fields = ('comentario','usuario','producto',)
 
     def get_success_url(self):
-        #si me logueo bien me manda a donde?
-        return reverse('indice') 
+        return "/detalle_producto/{}/".format(self.object.producto.pk)
 
-class RegistroUsuarios(CreateView):
+class Salir(LogoutView):
+	next_page = reverse_lazy('indice')
+
+class Ingresar(LoginView):
+	template_name = 'login.html'
+
+	def get(self, request, *args, **kwargs):
+		if request.user.is_authenticated:
+			return HttpResponseRedirect(reverse('indice'))
+		else:
+			context = self.get_context_data(**kwargs)
+			return self.render_to_response(context)
+
+	def get_success_url(self):
+		return reverse('indice')
+
+class CambiarPerfil(LoginRequiredMixin, UpdateView):
     model = User
-    template_name = 'registrar.html'
-    form_class = FormularioRegistro
-    success_url = reverse_lazy('ingresar')
-
-
-# cambio de perfil, solo si es logueado
-class ModificarPerfil(LoginRequiredMixin, UpdateView):
-    model = User
-    fields = ('telefono','last_name','first_name','email')
+    fields = ('telefono','last_name','first_name','email',)
     success_url = '/'
     template_name = 'perfil.html'
 
-    #usamos getobject para obtener el ibjeto a modificar
-    def get_object(self, queryser=None):
-        #esta función nis toma el obhjeto logeado.
+    def get_object(self, queryset=None):
         return self.request.user
 
-class AgregarCarrito(LoginRequiredMixin, CreateView):
-    #con esto añadimos al carrito
-    #llamamos el modelo carrito, los datos a llenar 
-    model = Carrito
+
+class AniadirCarrito(LoginRequiredMixin, CreateView):
+    model = CarritoCompras
     fields = ('usuario','producto','precio',)
-    #si todo va bien vamos a 
-    success_url = reverse_lazy('listadoProductos')
-    #si no estamos logueados logear
+    success_url = reverse_lazy('indice')
     login_url = 'ingresar'
 
-class EliminarCarrito(LoginRequiredMixin, DeleteView):
-    #con el podemos borrar el carrito cuando comprado es falso
-    queryset = Carrito.objects.filter(comprado=False)
-    model = Carrito
-    #si lo borra vamos a prodcutos
-    success_url = reverse_lazy('listadoProductos')
-    #si no estamos logueados que se loguie
+
+class EliminarCarrito(LoginRequiredMixin,DeleteView):
+    queryset = CarritoCompras.objects.filter(comprado=False)
+    model = CarritoCompras
+    success_url = reverse_lazy('indice')
     login_url = 'ingresar'
 
-class ListarCarrito(LoginRequiredMixin, ListView):
+class ListarCarrito(LoginRequiredMixin,ListView):
     template_name = 'carrito.html'
-    model = Carrito
-    #si es no esta comprado y no esta pendiente
-    queryset = Carrito.objects.filter(comprado=False, pendiente=False)
+    model = CarritoCompras
+    queryset = CarritoCompras.objects.filter(comprado=False,pendiente=False)
     login_url = 'ingresar'
 
     def get_context_data(self, **kwargs):
-        #sobre cargamos carro de pendientes
         context = super(ListarCarrito, self).get_context_data(**kwargs)
-        # con el tab sin compra para que el template lo entienda
-        context["tab"] = 'sincomprar'
+        context['tab'] = 'sincomprar'
         return context
 
-class ListarCarritoPendiente(LoginRequiredMixin, ListView):
+class ListarCarritoPendientes(LoginRequiredMixin,ListView):
     template_name = 'carrito.html'
-    model = Carrito
-    #si es pendiente y falsa de comprar lo mostramos en pendiente
-    queryset = Carrito.objects.filter(comprado=False, pendiente=True)
+    model = CarritoCompras
+    queryset = CarritoCompras.objects.filter(comprado=False,pendiente=True)
     login_url = 'ingresar'
 
     def get_context_data(self, **kwargs):
-        #sobre cargamos carro de pendientes
-        context = super(ListarCarritoPendiente, self).get_context_data(**kwargs)
-        # con el tab pendiente que el template lo entienda
-        context["tab"] = 'pendientes'
+        context = super(ListarCarritoPendientes, self).get_context_data(**kwargs)
+        context['tab'] = 'pendientes'
         return context
 
-class ListarCarritoFinalizado(LoginRequiredMixin, ListView):
+class ListarCarritoFinalizadas(LoginRequiredMixin,ListView):
     template_name = 'carrito.html'
-    model = Carrito
-    #cuando ya esta pago
-    queryset = Carrito.objects.filter(comprado=True, pendiente=False)
+    model = CarritoCompras
+    queryset = CarritoCompras.objects.filter(comprado=True,pendiente=False)
     login_url = 'ingresar'
 
     def get_context_data(self, **kwargs):
-        #sobre cargamos carro de pendientes
-        context = super(ListarCarritoFinalizado, self).get_context_data(**kwargs)
-        # con el tab finalizado para que el template lo entienda
-        context["tab"] = 'finalizadas'
+        context = super(ListarCarritoFinalizadas, self).get_context_data(**kwargs)
+        context['tab'] = 'finalizadas'
         return context
-    
+
 
 ########## pasarela de pagos #####################
 from django.utils.decorators import method_decorator
